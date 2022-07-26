447c828e6c26f443969ce46183f81501e6edcc46
Miguel <miguelclavijo03@gmail.com>
2022-07-26 17:25:22 -0500

    Ultima actualización

---
+++ index.js
@@ -0,0 +1,22 @@
+function encriptarTexto(text){
+    return text.split("").reverse().join("")
+}
+
+
+let textarea = document.getElementById("text-area")
+let parrafo = document.getElementById("parrafo")
+
+let initial = document.getElementById("initial")
+let final = document.getElementById("final")
+
+final.style.display = "none" 
+
+let botonEncriptar = document.getElementById("encriptar")
+botonEncriptar.addEventListener('click',() => {
+    console.log("Me hicieron click", textarea.value)
+    parrafo.innerText = textarea.value 
+    initial.style.display = "none";
+    final.style.display = "flex";
+})
+
+
