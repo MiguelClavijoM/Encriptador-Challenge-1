354f83272651398c178dea1e1c8f0168b6a680b0
Miguel <miguelclavijo03@gmail.com>
2022-07-28 15:11:52 -0500

    Finalizado

--- index.js
+++ index.js
@@ -1,25 +1,79 @@
 function encriptarTexto(text){
-    return text.split("").reverse().join("")
+    return text
+    .replaceAll("e", "enter") 
+    .replaceAll("i", "imes") 
+    .replaceAll("a", "ai") 
+    .replaceAll("o", "ober") 
+    .replaceAll("u", "ufat");
 }
 
+function desencriptarTexto(text){
+    return text
+    .replaceAll("enter", "e") 
+    .replaceAll("imes", "i") 
+    .replaceAll("ai", "a") 
+    .replaceAll("ober", "o") 
+    .replaceAll("ufat", "u");
+}
 
-let textarea = document.getElementById("text-area")
-let parrafo = document.getElementById("parrafo")
+let botonDesencriptar = document.getElementById("desencriptar");
 
-let initial = document.getElementById("initial")
-let final = document.getElementById("final")
+let textarea = document.getElementById("text-area");
+let parrafo = document.getElementById("parrafo");
+
+let initial = document.getElementById("initial");
+let final = document.getElementById("final");
 
 final.style.display = "none" 
 
-let botonEncriptar = document.getElementById("encriptar")
+let botonEncriptar = document.getElementById("encriptar");
 
 botonEncriptar.addEventListener('click',() => {
-    console.log("Me hicieron click", textarea.value)
-    parrafo.innerText = textarea.value
+    parrafo.value = encriptarTexto(textarea.value) 
     initial.style.display = "none";
     final.style.display = "flex";
 })
 
-let ejecutar = encriptarTexto(parrafo.innerText);
-console.log(ejecutar);
+botonDesencriptar.addEventListener('click',() => {
+    parrafo.value = desencriptarTexto(textarea.value)
+    initial.style.display = "none";
+    final.style.display = "flex";
+})
+
+let botonCopiar = document.getElementById("copiar");
+
+function copiarTexto() {
+    parrafo.select();
+    navigator.clipboard.writeText(parrafo.value);
+}
+
+botonCopiar.addEventListener('click', () => {
+    copiarTexto();
+    botonCopiar.innerText = "Texto copiado!"
+    setTimeout(()=>{
+        botonCopiar.innerText = "Copiar"
+    }, 1000)
+})
+
+function soloLetras(e)
+{
+key = e.keyCode || e.which;
+tecla = String.fromCharCode(key).toString();
+letras = "abcdefghijklmnopqrstuvwxyz";
+
+especiales = [8,13,32];
+tecla_especial = false
+for(var i in especiales) {
+if(key == especiales[i]){
+ tecla_especial = true;
+ break;
+}
+}
+
+if(letras.indexOf(tecla) == -1 && !tecla_especial)
+{
+ alert("Ingresar sólo letras minúsculas y sin acentos");
+ return false;
+}
+}
 
